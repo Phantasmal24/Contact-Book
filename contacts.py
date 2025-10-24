f44716a8bd098823d524a0c8f3dfa60266fdde55
Phantasmal24 <aditykr241106@gmail.com>
2025-10-24 23:29:23 +0530

    Feat: Add search_contact function

--- contacts.py
+++ contacts.py
@@ -37,6 +37,18 @@ def add_contacts(contacts):
     save_contacts(contacts)
     print(f"Contact '{name}' added successfully.")
 
+def search_contact(contacts):
+    """Searches for a contact by name."""
+    search_name = input("Enter the name you are searhing for: ")
+
+    if search_name in contacts:
+        details = contacts[search_name]
+        print("\n--- Contact Found ---")
+        print(f"Name: {search_name}, Phone: {details['phone']}, Email: {details['email']}")
+        print("---------------------\n")
+    else:
+        print(f"No contact found with this name '{search_name}'.\n")
+
 def main():
     """Main function to run the contact book app."""
     contacts = load_contacts()
@@ -45,15 +57,18 @@ def main():
         print("\nContact Book Menu:")
         print("1. View Contacts")
         print("2. Add a new Contact")
-        print("3. Exit")
+        print("3. Search for a contact")
+        print("4. Exit")
 
-        choice = input("Enter your choice (1/2/3): ")
+        choice = input("Enter your choice (1/2/3/4): ")
 
         if choice == "1":
             view_contacts(contacts)
         elif choice == "2":
             add_contacts(contacts)
         elif choice == "3":
+            search_contact(contacts)
+        elif choice == "4":
             print("Goodbye!")
             break
         else:
